05651a5c54d0d1121a9e6227c165c4fc5c56f7af
marboerq@gmail.com <marboerq@gmail.com>
2026-07-09 22:10:33 -0300

    teste de funcionamento no main

--- src/main/java/com/mycompany/pi2ativ04/PI2ativ04.java
+++ src/main/java/com/mycompany/pi2ativ04/PI2ativ04.java
@@ -6,12 +6,28 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import model.Produto;
+import service.ProdutoService;
 
 public class PI2ativ04 {
 
     
     public static void main(String[] args) {
-         
+         ProdutoService service = new ProdutoService();
+
+        Produto p = new Produto();
+
+        p.setNomeProduto("Teste");
+        p.setFabricante("Samsung");
+        p.setFornecedor("Fornecedor Teste");
+
+        service.cadastrarProduto(p);
+
+        System.out.println("Cadastro realizado com sucesso.");
+
+        service.listarProdutos()
+                .forEach(prod ->
+                        System.out.println(prod.getNomeProduto()));
      
     }
 }
